cdfba18f81a34eb6458fe25b8cefa21926850031
Damian Elenbaas <damianelenbaas2003@gmail.com>
2023-11-08 19:03:01 +0100

    Added navbar component

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FeaturesModule } from '@hour-master/frontend/features';
+import { UiModule } from '@hour-master/ui';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, FeaturesModule],
+  imports: [RouterModule, FeaturesModule, UiModule],
   selector: 'hour-master-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Hour Master';
 }
