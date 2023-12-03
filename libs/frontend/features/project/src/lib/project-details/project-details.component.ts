4601c7f28cc40a76db3ba7995aa8a25648eb3ad4
Damian Elenbaas <damianelenbaas2003@gmail.com>
2023-12-04 00:42:05 +0100

    Added project details page

--- libs/frontend/features/project/src/lib/project-details/project-details.component.ts
+++ libs/frontend/features/project/src/lib/project-details/project-details.component.ts
@@ -1,8 +1,71 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IProject } from '@hour-master/shared/api';
+import { ProjectService } from '../project.service';
+import { AuthService } from '@hour-master/frontend/auth';
+import { AlertService } from '@hour-master/frontend/common';
+import { Subscription, of, switchMap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'hour-master-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.scss'],
 })
-export class ProjectDetailsComponent {}
+export class ProjectDetailsComponent implements OnInit, OnDestroy {
+  sub!: Subscription;
+  project!: IProject;
+  totalHours = 0;
+  token!: string;
+
+  constructor(
+    public readonly location: Location,
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly authService: AuthService,
+    private readonly alertService: AlertService,
+    private readonly projectService: ProjectService) { }
+
+  ngOnInit(): void {
+    this.sub = this.authService.currentUserToken$
+      .pipe(
+        switchMap((token) => {
+          if (token) {
+            this.token = token;
+            return this.route.paramMap;
+          } else {
+            this.alertService.danger('Je bent niet ingelogd!');
+            this.router.navigate(['/auth/login']);
+            return of(null);
+          }
+        })
+      )
+      .pipe(
+        switchMap((params) => {
+          if (params) {
+            const id = params.get('id') as string;
+            return this.projectService.details(id, {
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + this.token,
+              },
+            });
+          } else {
+            return of(null);
+          }
+        })
+      )
+      .subscribe((project) => {
+        if (project) {
+          this.project = project;
+        } else {
+          this.alertService.danger('Project niet gevonden!');
+          this.router.navigate(['/projects']);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+}
