79685ca38b1e78bb005b623e9144b2d79a76b12b
Damian Elenbaas <damianelenbaas2003@gmail.com>
2023-12-08 14:33:26 +0100

    Fixed typo in environment variables

--- libs/shared/environments/src/lib/environment.prod.ts
+++ libs/shared/environments/src/lib/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   dataApiUrl: 'https://hour-master-data-api.azurewebsites.net',
-  rcmndApiUrl: 'https://hour-master-rcmd-api.azurewebsites.net',
+  rcmndApiUrl: 'https://hour-master-rcmd-api.azurewebsites.net/',
 };
